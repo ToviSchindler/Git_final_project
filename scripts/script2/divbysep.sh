aef16e75cae447ce5a8b290577747145e8d81a62
Rachel <rachel058326@gmail.com>
2023-08-07 16:11:37 +0300

    task2 completed by rachel gershon

---
+++ scripts/script2/divbysep.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+file="$1"
+separator="$2"
+
+if [ ! -f "$file" ]; then
+    echo "The file was not found"
+    exit 1
+fi
+
+while IFS= read -r line; do
+    IFS="$separator" read -ra fields <<< "$line"
+    for (( i = 0; i < ${#fields[@]}; i++ )); do
+        echo "field $((i+1)): ${fields[i]}"
+    done
+done < "$file"
+
+
