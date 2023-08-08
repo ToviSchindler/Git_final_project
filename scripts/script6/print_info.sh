78f54976c2c80c2bba1e6098a6a271bda498b29e
tovi <tovi@newUbuntu.myguest.virtualbox.org>
2023-08-08 14:37:54 +0300

    exc6 with log file

--- scripts/script6/print_info.sh
+++ scripts/script6/print_info.sh
@@ -9,6 +9,7 @@ print_user_info(){
   while IFS=":" read -r username x uid gid gecos home shell; do
     if [[ $shell == "/bin/bash" ]]; then
       printf "%-15s%-5s%-5s%-25s%-25s%s\n" "$username" "$uid" "$gid" "$gecos" "$home" "$shell"
+      printf "%-15s%-5s%-5s%-25s%-25s%s\n" "$username" "$uid" "$gid" "$gecos" "$home" "$shell" > log_file
     fi
   done < /etc/passwd
 }
